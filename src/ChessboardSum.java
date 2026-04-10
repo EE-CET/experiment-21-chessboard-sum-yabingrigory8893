2ae48af5698a2a2ae3c6d387c81c183f917264f4
yabingrigory8893 <yabingrigory3@gmail.com>
2026-04-10 01:50:35 -0700

    Update ChessboardSum.java

--- src/ChessboardSum.java
+++ src/ChessboardSum.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 public class ChessboardSum {
-    
-        // TODO: Read N
-        // TODO: Read the matrix
-        // TODO: Iterate through the matrix
-        // TODO: Check if (i + j) is even (Black) or odd (White)
-        // TODO: Accumulate sums and print them
-    
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            if (!scanner.hasNextInt()) return;
+            int n = scanner.nextInt();
+            long blackSum = 0;
+            long whiteSum = 0;
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (scanner.hasNextInt()) {
+                        int value = scanner.nextInt();
+                        if ((i + j) % 2 == 0) {
+                            blackSum += value;
+                        } else {
+                            whiteSum += value;
+                        }
+                    }
+                }
+            }
+
+            System.out.println(blackSum);
+            System.out.println(whiteSum);
+        }
+    }
 }
